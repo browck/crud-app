567bb8a6d3de0195822b3a85c19bea0a4cf3e018
Chris Brown <browck@gmail.com>
2016-02-21 00:34:39 -0500

    Basic Client CRUD operations available, need to join person to client

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -8,3 +8,23 @@ CREATE TABLE person (
     state varchar(2) NOT NULL,
     zip_code varchar(5) NOT NULL
 );
+
+CREATE TABLE client (
+    client_id integer IDENTITY,
+    company_name varchar(50) NOT NULL,
+    website varchar(50) NOT NULL,
+    phone_number varchar(50) NOT NULL,
+    street_address varchar(50) NOT NULL,
+    city varchar(50) NOT NULL,
+    state varchar(2) NOT NULL,
+    zip_code varchar(5) NOT NULL
+);
+
+CREATE TABLE person_client (
+    id integer IDENTITY,
+    person_id INTEGER NOT NULL,
+    client_id INTEGER NOT NULL,
+    FOREIGN KEY (person_id) REFERENCES person(person_id),
+    FOREIGN KEY (client_id) REFERENCES client(client_id)
+)
+
